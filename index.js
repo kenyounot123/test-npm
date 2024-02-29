ed833d2e42d19978e7c758f14da77a5f15cdc0be
kenyounot123 <kenlu519@gmail.com>
2024-02-28 22:00:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { newDropDownMenu } from "simpledrop-js/index.js";
+
+const btn = document.getElementsByClassName("menu");
+const dropdown = document.getElementsByClassName("dropdown");
+
+newDropDownMenu(btn, dropdown);
